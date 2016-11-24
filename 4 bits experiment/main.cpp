00f62fa0126fd035c6ddeed82a53ffeffe1e39e5
Voha888 <voha@wap-robin.com>
2016-11-24 21:26:14 +0200

    Created by Coding Ground Client

---
+++ 4 bits experiment/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   cout << "Hello World" << endl; 
+   
+   
+   int res = 21;
+    
+    cout << (bool) (res & 1) << endl;
+    cout << (bool) (res & 2) << endl;
+    cout << (bool) (res & 4) << endl;
+    cout << (bool) (res & 8) << endl;
+   
+   bool a=2;
+   
+   cout<< "Bit one   "<<a<< endl;
+   cout<< "Bit second  "<<"Bit one"<< endl;
+   cout<< "Bit third   "<<"Bit one"<< endl;
+   cout<< "Bit fourth   "<<"Bit one"<< endl;
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   return 0;
+}
+
